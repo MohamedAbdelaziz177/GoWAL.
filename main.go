1c847989360869c0d7ec1ee6a537198957951966
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-02-12 19:20:23 +0200

    Testing Checkpoint without truncating WAL

--- main.go
+++ main.go
@@ -2,8 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	cleanPrevFiles()
+
+	kv, err := NewKVStore("data/demo.wal")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 1; i <= 15; i++ {
+		key := fmt.Sprintf("key%d", i)
+		value := fmt.Sprintf("value%d", i)
+
+		if err := kv.Set(key, value); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Size after delete:", kv.Size())
+
+	fmt.Println("Creating checkpoint...")
+
+	if err := kv.CheckPointWithoutTruncating("data/snapshot.db"); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Checkpoint created. WAL truncated.")
+
+	if err := kv.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func cleanPrevFiles() {
+
+	os.MkdirAll("data", 0755)
+	os.Remove("data/demo.wal")
+	os.Remove("data/snapshot.db")
 }
