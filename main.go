29a019237d706ee90ffc937c9f805f537f6c6f0f
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-02-14 17:50:49 +0200

    Simulating Crash & testing WAL recovery

--- main.go
+++ main.go
@@ -25,20 +25,54 @@ func main() {
 		}
 	}
 
-	fmt.Println("Size after delete:", kv.Size())
+	fmt.Println("new Size after delete:", kv.Size())
 
-	fmt.Println("Creating checkpoint...")
-
-	if err := kv.CheckPointWithoutTruncating("data/snapshot.db"); err != nil {
+	fmt.Println("Checkpointing ...")
+	if err := kv.Checkpoint("data/snapshot.db"); err != nil {
 		log.Fatal(err)
 	}
 
+	/*
+		if err := kv.CheckPointWithoutTruncating("data/snapshot.db"); err != nil {
+			log.Fatal(err)
+
+		}
+	*/
+
 	fmt.Println("Checkpoint created. WAL truncated.")
 
+	if err := kv.Set("key16", "value16"); err != nil {
+		log.Fatalf("Failed to set key16: %v", err)
+	}
+	if err := kv.Delete("key1"); err != nil {
+		log.Fatalf("Failed to delete key1: %v", err)
+	}
+
+	val, _ := kv.Get("key16")
+	fmt.Printf("Current State before crash: key16 = %s, Total Size: %d\n", val, kv.Size())
+
+	/* اعتبؤها كراش مثلا */
 	if err := kv.Close(); err != nil {
 		log.Fatal(err)
 	}
 
+	/* here, it recovers after the crash  */
+	newKv, err := NewKVStore("data/demo.wal")
+
+	if err != nil {
+		log.Fatalf("Failed to recover: %v", err)
+	}
+
+	defer newKv.Close()
+
+	val16, exists16 := newKv.Get("key16")
+	val1, exists1 := newKv.Get("key1")
+
+	fmt.Println("Results after WAL recovery:")
+
+	fmt.Printf("key16 exists: %v, value: %s\n", exists16, val16)
+
+	fmt.Printf("key1 exists? : %v, value: %s\n", exists1, val1)
 }
 
 func cleanPrevFiles() {
